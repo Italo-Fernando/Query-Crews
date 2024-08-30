86443cec5467fc5cff7e69b7fba3911a8d1a790c
LucasGCRamos <117207058+LucasGCRamos@users.noreply.github.com>
2024-08-29 22:51:27 -0300

    feat: Criação do main e import subprocess

--- local_connection.py
+++ local_connection.py
@@ -1,5 +1,3 @@
-import mysql.connector
-
 class Database_connection:
     def __init__(self):
         self.__user = None  # User object
@@ -33,29 +31,3 @@ class Database_connection:
         database = input("Digite o nome do banco de dados(Schema): ")
         self.database = database
         return self.database
-
-def main():
-    db = Database_connection()
-    user = db.add_user()
-    password = db.add_password()
-    host = db.add_host()
-    port = db.add_port()
-    database = db.add_database()
-    
-    config = {
-        'user': user,
-        'password': password,
-        'host': host,
-        'port': port,
-        'database': database
-    }
-
-    try:
-        conexao = mysql.connector.connect(**config)
-        if conexao.is_connected():
-            print("Conectado ao banco de dados com sucesso!")
-    except mysql.connector.Error as e:
-        print(f"Erro ao conectar ao banco de dados: {e}")
-
-if __name__ == '__main__':
-    main()
